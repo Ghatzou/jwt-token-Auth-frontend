8a6148e796e1dcca5cd2f3210761c77c0358adef
TAGHEZOUIT amine <aminetaghezouitbus@hotmail.com>
2021-04-02 01:15:08 +0100

    login submit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,48 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/Services/auth.service';
-import { HttpRoutesService } from 'src/app/Services/http-routes.service';
-import { TokenService } from 'src/app/Services/token.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  //variable declaration 
+  //variable declaration
   public form = {
-    email:null,
-    password:null
+    email: null,
+    password: null,
   };
 
   //methodes
   constructor(
-    private httpRoutesService: HttpRoutesService,
-    private token: TokenService,
-    private router: Router,
     private auth: AuthService
-  ){ 
+  ) {
     //
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {  }
 
   onSubmit() {
-    return this.httpRoutesService.login(this.form).subscribe(
-     data => this.handleResponse(data),
-     error => console.log(error)
-   );
+    this.auth.login(this.form);
   }
-
-  handleResponse(data) {
-    let isValidToken = this.token.handle(data.access_token);
-    if(isValidToken) {
-      this.auth.changeAuthStatus(true);
-      this.router.navigateByUrl('/home');
-    }
-  }
-
 }
