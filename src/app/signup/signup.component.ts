0cf223b5c58f980c99282cf44b0a9310b4c2c871
TAGHEZOUIT amine <aminetaghezouitbus@hotmail.com>
2021-04-02 01:15:36 +0100

    signup post

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,42 +1,42 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
 import { HttpRoutesService } from 'src/app/Services/http-routes.service';
-import { TokenService } from 'src/app/Services/token.service';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
-  //variable declaration 
+  //variable declaration
   public form = {
     first_name: null,
     last_name: null,
     email: null,
     password: null,
-    password_confirmation: null
+    password_confirmation: null,
   };
 
   public error: any = [];
+  private RegisterSubmitSubscription : Subscription = null;
 
   //methodes
   constructor(
     private httpRoutesService: HttpRoutesService,
-    private token: TokenService,
+    private auth: AuthService,
     private router: Router
   ) {
     //
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-    return this.httpRoutesService.register(this.form).subscribe(
-      data => this.handleResponse(data),
+    this.RegisterSubmitSubscription = this.httpRoutesService.register(this.form).subscribe(
+      (data) => this.handleResponse(data),
       (error) => {
         this.error = error.error.errors;
         console.log(this.error);
@@ -45,9 +45,16 @@ export class SignupComponent implements OnInit {
   }
 
   handleResponse(data) {
-    if (this.token.handle(data.access_token)) {
-      this.router.navigateByUrl('/home')
-    }
+    //tmp solution
+    this.router.navigate(['home']);
+    //i don't know what i should do here, when i cam to registration i will change it
+    // this.auth.login(data.access_token);
+    // this.auth.isLoggedInAsObservable().pipe(
+    //   map((loggedIn) => {
+    //     if (loggedIn) {
+    //       this.router.navigate(['home']);
+    //     }
+    //   })
+    // );
   }
-
 }
